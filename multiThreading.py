88df8643bea2145318f59523fed5ce058d31075a
Papa Beye <32583389+PapaBeye@users.noreply.github.com>
2018-11-15 18:00:14 -0500

    unfinished code commit

--- multiThreading.py
+++ multiThreading.py
@@ -1,5 +1,6 @@
 import cv2
 import threading
+import os
 import numpy as np
 import serial
 import imutils
@@ -8,7 +9,7 @@ import VACSParser
 import time
 
 if len(sys.argv) != 5:
-    print("Usage: python multiThreading.py station-ID gspiport fcport message_definition_path")
+    print("Usage: python multiThreading.py station-ID gsPIport fcPIport message_definition_path")
     sys.exit(1)
 else:
     stid = sys.argv[1]
@@ -25,12 +26,12 @@ class mainsys:
             self.gscomtopi = serial.Serial(str(gscomport), baudrate=57600, parity=serial.PARITY_NONE, 
                                            stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
         if stid == 'PI':
-            #self.gscomtopi = serial.Serial(str(gscomport), baudrate=57600, parity=serial.PARITY_NONE,
-            #                               stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-            #self.fccomport = serial.Serial(str(fccomport), baudrate=57600,)
+            self.gscomtopi = serial.Serial(str(gscomport), baudrate=57600, parity=serial.PARITY_NONE,
+                                           stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+            self.fccomport = serial.Serial(str(fccomport), baudrate=57600,)
             self.vcap = cv2.VideoCapture(0)
             self.isimg_frame = self.vcap.read()
-            #self.message_definition_path = message_definition_path
+            self.message_definition_path = message_definition_path
             self.turncamoff = self.vcap.release()
 
 
@@ -38,26 +39,46 @@ class rpisys(mainsys):
 
     def find_whiterec_fame(self):
         while 1:
+            # self.isimg_frame
             _, img = self.isimg_frame
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+            print("made it to tresh\n")
             thresh = cv2.threshold(blurred, 230, 255, cv2.THRESH_BINARY)[1]
             cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+            print("made it to cnts\n")
             for c in cnts:
                 M = cv2.moments(c)
                 peri = cv2.arcLength(c, True)
                 approx = cv2.approxPolyDP(c, 0.04 * peri, True)
                 area = cv2.contourArea(c)
                 if len(approx) == 4:
+                    print("its has 4 sides\n")
                     (x, y, w, h) = cv2.boundingRect(approx)
                     r = w / float(h) # 7000 is arbitrary for this purpose its the area of the larget object on test image / 2
-                    if area > 2000 and r >= 0.60 and r <= 1.30:
+                    if area > 2000 and r >= 0.70 and r <= 1.30:
                         cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
-                        if not os.path.exists('positive'):
-                            os.makedirs('positive')
-                        name = time.time * 1000
-                        cv2.imwrite("positive/img"+str(name)+".jpg", img)
+                        print("passed to handle\n")
+                        self.handleimg(img, 1)
+                        time.sleep(5)
+                else:
+                    self.handleimg(img, 0)
+                    time.sleep(5)
+    def handleimg(self, image, xint):
+        if xint == 1:
+            if not os.path.exists('positive'):
+                os.makedirs('positive')
+            name = time.time() * 1000
+            cv2.imwrite("positive/img"+str(name)+".jpg", image)
+        elif xint == 0:
+            if not os.path.exists('negative'):
+                os.makedirs('negative')
+            name = time.time() * 1000
+            cv2.imwrite("negative/img"+str(name)+".jpg", image)
+            
+                    
+
 '''
             cv2.imshow('img', img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
